8d90c39276bb04fb3d92c59b0f66dd2fbd61fad4
ABHIJITH <aabhivishnu@gmail.com>
2024-04-03 12:37:31 +0530

    second commit

--- API_GATEWAY/src/main/java/com/API_GATEWAY/API_GATEWAY/filter/RouteValidator.java
+++ API_GATEWAY/src/main/java/com/API_GATEWAY/API_GATEWAY/filter/RouteValidator.java
@@ -1,29 +1,25 @@
 package com.API_GATEWAY.API_GATEWAY.filter;
 
 
-import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
 import java.util.List;
-import java.util.function.Predicate;
 
 
 @Component
 public class RouteValidator {
 
-
-    public static final List<String> openApiEndpoints=List.of(
-            "api/v1/auth/register",
-            "api/v1/auth/login",
+    public static final List<String> openApiEndpoints = List.of(
+            "/api/v1/auth/register",
+            "/api/v1/auth/login",
+            "/api/v1/auth/otp",
             "/test/yes",
             "/eureka"
-
     );
 
-
-    public Predicate<ServerHttpRequest> isSecured=
-            request->openApiEndpoints
-                    .stream()
-                    .noneMatch(uri->request.getURI().getPath().contains(uri));
-
+    public boolean isSecured(URI requestUri) {
+        String requestPath = requestUri.getPath();
+        return openApiEndpoints.stream().noneMatch(requestPath::equals);
+    }
 }
